c5997aa210d871e991f865d96bdd147c7e5ee0cd
DevJhonny <miranda3791167@gmail.com>
2024-08-18 11:25:00 -0500

    CREANDO LA FUNCION CRUD PERSONA, FUNCION DE INSERTAR

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-define('db', 'db_agenda24');
-define('usuario', 'root');
-define('clave', '');
-define('host', 'localhost');
+define('db','db_agenda24');
+define('usuario','root');
+define('clave','');
+define('host','localhost:3306');
 
 $mysqli = new mysqli(host, usuario, clave, db);
--- crud_persona.php
+++ crud_persona.php
@@ -2,6 +2,40 @@
 include('config.php');
 $post = json_decode(file_get_contents("php://input"), true);
 
+// //FUNCION INSERTAR
+// if ($post['accion'] == "insertar") {
+//     $sentencia = sprintf("INSERT INTO persona (ci_persona, nom_persona, ape_persona, clave_persona, correo_persona)values('%s', '%s', '%s', '%s', '%s')",$post = ['cedula'], $post = ['nombre'], $post = ['apellido'], $post = ['clave'], $post = ['email']);
+//     $result = mysqli_query($conexion, $sentencia);
+//     if ($result) {
+//         $respuesta = json_encode(array('estado' => true, "mensaje" => "DATOS:GUARDADOS CORRECTAMENTE"));
+//     } else {
+//         $respuesta = json_encode(array('estado' => false, "mensaje" => "ERROR: DATOS NO GUARDADOS"));
+//     }
+//     echo $respuesta;
+// }
+
+if ($post['accion'] == "insertar") {
+    $sentencia = sprintf(
+        "INSERT INTO persona (ci_persona, nom_persona, ape_persona, clave_persona, correo_persona) VALUES ('%s', '%s', '%s', '%s', '%s')",
+        $post['cedula'],
+        $post['nombre'],
+        $post['apellido'],
+        $post['clave'],
+        $post['correo']
+    );
+    $result = mysqli_query($mysqli, $sentencia);
+    if ($result) {
+        $respuesta = json_encode(array('estado' => true, "mensaje" => "Datos Guardados Correctamente"));
+    } else {
+        $respuesta = json_encode(array('estado' => false, "mensaje" => "Error al guardar"));
+    }
+    echo $respuesta;
+}
+
+
+
+
+//FUNCION CONSULTAR
 if ($post['accion'] == "consultar") {
     $sentencia = sprintf("SELECT* FROM persona");
     $result = mysqli_query($mysqli, $sentencia);
@@ -11,7 +45,7 @@ if ($post['accion'] == "consultar") {
                 'codigo' => $row['cod_persona'],
                 'cedula' => $row['ci_persona'],
                 'nombre' => $row['nom_persona'],
-                'apellido' => $row['ape_persona'],
+                'apellido' => $row['ape_persona']
             );
         }
         $respuesta = json_encode(array('estado' => true, "personas" => $datos));
